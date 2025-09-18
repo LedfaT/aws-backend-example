87aa3cb38b5a3ebc50c9de1d50ba1b0db2a20605
David Zaitsev <davidgalanix@gmail.com>
2025-09-18 11:00:39 +0300

    deploy test

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ config();
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-const pgPool = new Pool({
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: process.env.POSTGRES_PORT || 5432,
-  user: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "password",
-  database: process.env.POSTGRES_DB_NAME || "testdb",
-});
+// const pgPool = new Pool({
+//   host: process.env.POSTGRES_HOST || "localhost",
+//   port: process.env.POSTGRES_PORT || 5432,
+//   user: process.env.POSTGRES_USER || "postgres",
+//   password: process.env.POSTGRES_PASSWORD || "password",
+//   database: process.env.POSTGRES_DB_NAME || "testdb",
+// });
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_PORT || 6379,
-});
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST || "localhost",
+//   port: process.env.REDIS_PORT || 6379,
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello from Express on AppRunner 🚀");
@@ -31,24 +31,24 @@ app.get("/health", (req, res) => {
   res.send("OK");
 });
 
-app.get("/db", async (req, res) => {
-  try {
-    const result = await pgPool.query("SELECT NOW()");
-    res.json({ time: result.rows[0] });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// app.get("/db", async (req, res) => {
+//   try {
+//     const result = await pgPool.query("SELECT NOW()");
+//     res.json({ time: result.rows[0] });
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
-app.get("/cache", async (req, res) => {
-  try {
-    await redis.set("hello", "world", "EX", 60); // TTL 60 сек
-    const value = await redis.get("hello");
-    res.json({ hello: value });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// app.get("/cache", async (req, res) => {
+//   try {
+//     await redis.set("hello", "world", "EX", 60); // TTL 60 сек
+//     const value = await redis.get("hello");
+//     res.json({ hello: value });
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
